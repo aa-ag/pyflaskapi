952fee0fb4dfd4444ae3c9fe50432bde6939c853
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-26 18:55:24 -0600

    added temporary store list with book objects

--- server.py
+++ server.py
@@ -5,6 +5,24 @@ from flask import Flask
 ############------------ GLOBAL VARIABLE(S) ------------############
 app = Flask(__name__)
 
+books = [
+    {
+        "id": 1,
+        "title": "Book 1",
+        "author": "Author 1"
+    },
+    {
+        "id": 2,
+        "title": "Book 2",
+        "author": "Author 2"
+    },
+    {
+        "id": 3,
+        "title": "Book 3",
+        "author": "Author 3"
+    }
+]
+
 ############------------ FUNCTION(S) ------------############
 @app.route("/")
 def hello_world():
