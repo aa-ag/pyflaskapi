f21a7a2ecd646eed3bc6af9b03ad191905ee8ddb
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-27 13:02:12 -0600

    corrected import of books_blueprint within inside wht create_app function & added app.config of track modifications = False

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 ############------------ IMPORTS ------------############
 from flask import Flask
-from app.books.books_blueprint import books_blueprint
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -9,10 +8,13 @@ db = SQLAlchemy()
 
 ############------------ FUNCTION(S) ------------############
 def create_app():
+    from app.books.books_blueprint import books_blueprint
     # create a flask app
     app = Flask(__name__)
     # create/config sqlite database
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+    # set track modifications to False
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     # extend the books_blueprint object 
     # to consume encapsulated functionality
     app.register_blueprint(books_blueprint)
