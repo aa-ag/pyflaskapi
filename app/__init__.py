0b2ac743ad83ffe29f00425316a2669b633e52e7
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-27 12:12:02 -0600

    added comments & config function to create_app() function to set up and use sqlite database for the app

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,16 @@ from app.books.books_blueprint import books_blueprint
 from flask_sqlalchemy import SQLAlchemy
 
 
+############------------ GLOBAL VARIABLE(S) ------------############
+db = SQLAlchemy()
+
 ############------------ FUNCTION(S) ------------############
 def create_app():
+    # create a flask app
     app = Flask(__name__)
+    # create/config sqlite database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://books.db'
+    # extend the books_blueprint object 
+    # to consume encapsulated functionality
     app.register_blueprint(books_blueprint)
     return app
