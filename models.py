6dde828b458f19718d9fa8bbaed119c902265042
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-27 12:20:02 -0600

    added serialized method within Book class

--- models.py
+++ models.py
@@ -7,3 +7,9 @@ class Book(db.Model):
     id_ = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
     author = db.Column(db.String(50), nullable=False)
+
+    def serialized(self):
+        json = {
+            "title": self.title,
+            "author": self.author
+        }
