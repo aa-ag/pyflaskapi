7bf2db1d81a40cbc8ddc03c463f5ea0fd41cd7c2
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-27 11:36:48 -0600

    refactored to create app calling it from books/__init__.py and run at specific host/port

--- server.py
+++ server.py
@@ -1,73 +1,8 @@
 ############------------ IMPORTS ------------############
-from flask import Flask, jsonify, request
+from app import create_app
 
 
 ############------------ GLOBAL VARIABLE(S) ------------############
-app = Flask(__name__)
-
-books_store = [
-    {
-        "id": 1,
-        "title": "Book 1",
-        "author": "Author 1"
-    },
-    {
-        "id": 2,
-        "title": "Book 2",
-        "author": "Author 2"
-    },
-    {
-        "id": 3,
-        "title": "Book 3",
-        "author": "Author 3"
-    }
-]
-
-############------------ ENDPOINT(S) ------------############
-### HOME ####################################################
-@app.route("/")
-def home():
-    return "Update the url ^"
-
-
-### GET #####################################################
-@app.route("/books", methods=["GET"])
-def get_books():
-    serialized = {"books": books_store}
-    return jsonify(serialized)
-
-
-@app.route("/books/<int:uid>", methods=["GET"])
-def get_book(uid):
-    requested_book = next(book for book in books_store if book["id"] == uid)
-    return jsonify(requested_book)
-
-
-### POST ####################################################
-@app.route("/books", methods=["POST"])
-def post_book():
-    request_json = request.get_json()
-
-    try:
-        book_title = request_json["title"]
-    except KeyError:
-        return "Please provide a title", 400
-
-    try:
-        book_author = request_json["author"]
-    except KeyError:
-        return "Please provide an author", 400
-
-    new_book = {
-        "id": books_store[-1]["id"] + 1,
-        "title": book_title,
-        "author": book_author,
-    }
-
-    books_store.append(new_book)
-
-    serialized = {
-        "books": books_store
-    }
-
-    return jsonify(serialized)
+if __name__ == "__main__":
+    app = create_app()
+    app.run(host='0.0.0.0', port=5000)
