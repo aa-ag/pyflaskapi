0d01396d76d6bc572790762f8e065b562067db2a
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-27 12:19:17 -0600

    modeled the Book class

--- models.py
+++ models.py
@@ -4,4 +4,6 @@ from app import db
 
 ############------------ CLASS(ES) ------------############
 class Book(db.Model):
-    pass
+    id_ = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    author = db.Column(db.String(50), nullable=False)
